572a0739a67e3edd171679c875318aebae07e238
Osiryu <74241584+Osiryu@users.noreply.github.com>
2020-11-21 10:55:24 +0100

    Header v0.1

---
+++ header.php
@@ -0,0 +1,23 @@
+<!--
+Header for Atomic Vidéo created by Léo Delobel & Antoine Gangloff
+All credit goes for them
+-->
+
+<html>
+<head>
+
+</head>
+<body>
+  <div class="background">
+    <div class="name">
+    </div>
+    <div class="search">
+      <form method="GET">
+        <input type="text" name="search" class="search_bar">
+        <input type="button" name="start_search_button" class="button_search">
+      </form>
+    </div>
+    <div class="profile">
+    </div>
+</body>
+</html>
