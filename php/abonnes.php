e2f83f59f48694ca0c4dd14ba8076bdfa522d7ab
LeoDelobel <elobel.leo@gmail.com>
2021-02-23 16:33:01 +0100

    Ajout GetAbonnes()

---
+++ php/abonnes.php
@@ -0,0 +1,10 @@
+<?php
+  function GetAbonnes($id_utilisateur){
+    require("init_sql.php");
+    $statement = $DATABASE->prepare("SELECT COUNT(id_master) AS nombre FROM abonner WHERE id_master = ?");
+    $statement->execute(array($id_utilisateur));
+    $compte = $statement->fetchAll()[0];
+
+    return $compte["nombre"];
+  }
+ ?>
--- php/profil.php
+++ php/profil.php
@@ -4,13 +4,14 @@
     require_once("class_user.php");
 
     $utilisateur = UserManager::FindUser($id_utilisateur);
+    require_once("abonnes.php");
     ?>
 
     <span class="profil">
       <img class="profil_img" src="res/miniatures/<?php echo $video["id_video"] ?>.jpg">
       <div class="profil_meta">
         <p class="profil_nom"> <?php echo $utilisateur->pseudonyme ?></p>
-        <p class="profil_abonnes"> <?php echo 0 ?> abonnés</p>
+        <p class="profil_abonnes"> <?php echo GetAbonnes($id_utilisateur) ?> abonnés</p>
         <button class="abonnement"> S'abonner </p>
       </div>
     </span>
