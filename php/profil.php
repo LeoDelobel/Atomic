bcc7b7d59681c2a8a05c45244c5c96342d5f90ee
LeoDelobel <elobel.leo@gmail.com>
2021-02-23 14:28:16 +0100

    Correction

--- php/profil.php
+++ php/profil.php
@@ -10,14 +10,12 @@
     <span class="profil">
       <img class="profil_img" src="res/miniatures/<?php echo $video["id_video"] ?>.jpg">
       <div class="profil_meta">
-        <p class="profil_nom"> <?php echo $video["titre"] ?></p>
-        <p class="profil_abonnes"> <?php echo $video["nombre_vues"] ?> vues</p>
-        <button class="abonnement"> <?php echo $auteur["pseudonyme"] ?></p>
+        <p class="profil_nom"> <?php echo $utilisateur["pseudonyme"] ?></p>
+        <p class="profil_abonnes"> <?php echo 0 ?> abonnés</p>
+        <button class="abonnement"> S'abonner </p>
       </div>
     </span>
 
     <?php
   }
 ?>
-  }
- ?>
