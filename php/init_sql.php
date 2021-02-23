075ee5dcbbcd2f9653777a9045b15d6f070257a4
LeoDelobel <elobel.leo@gmail.com>
2021-02-23 13:54:26 +0100

    Correction

--- php/init_sql.php
+++ php/init_sql.php
@@ -1,7 +1,8 @@
 <?php
+  try{
   $DATABASE = new PDO('mysql:dbname=atomic;host=127.0.0.1','atomic','NK5SjKJj');
   $DATABASE->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+  } catch (PDOException $e) {
     echo 'Échec lors de la connexion : ' . $e->getMessage();
-}
+  }
  ?>
