bddb524ad460daac22da24525018173dfaf2ad3e
LeoDelobel <elobel.leo@gmail.com>
2021-03-09 14:23:28 +0100

    Redirection après abonnement

--- php/validation.php
+++ php/validation.php
@@ -3,4 +3,5 @@
   require_once("abonnes.php");
   echo $_SESSION["id_utilisateur"];
   AddAbonnement(htmlspecialchars($_GET["id_master"]),$_SESSION["id_utilisateur"]);
+  header('Location: ../index.php');
 ?>
