f6bc400c14ce5b4e1395b6133ada831117cdd62b
Osiryu <osirixel@gmail.com>
2021-03-09 15:28:08 +0100

    Css profil

--- header.php
+++ header.php
@@ -7,26 +7,28 @@
   <link rel ="stylesheet" href = "css/style_header.css"/>
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-  <div class="container-fluid header"> <!-- header entier class=header -->
-    <div class="row allelements"> <!-- Tout les éléments (allelements) -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div class="header_background">
+      
+    </div>
+  </body>
+</html>
 
-      <div class="col-sm-1" id="lgAV_area">
-        <img src="img/logo.png" id="lgAV"/>
-      </div>
 
-      <div class="col-sm-auto" id="titleAV  ">
-        <p><a href="index.php">Atomic Vidéo</a></p>
-      </div>
+
+
       <form action="search.php" type="get">
         <input type="text" id="search_bar" name="q" />
         <input type="submit" id="search_button" src="ressources/profile" value="Rechercher"/>
       </form>
 
-        <div class="row grey_area">
-          <img src="img/profile.jpeg" id="profile_picture" />
-          <p id="account_name_area"><?php
+<?php
 
           # Si l'utilisateur est connecté, $_SESSION["auth"] est true
           session_start();
