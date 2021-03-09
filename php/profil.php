efc512f42fb76d3f5feb7b61434948da7a61232d
Osiryu <osirixel@gmail.com>
2021-03-09 15:29:26 +0100

    Css profil

--- php/profil.php
+++ php/profil.php
@@ -1,23 +1,25 @@
-<?php
-  function PrintProfil($id_utilisateur){
-    include("init_sql.php");
-    require_once("class_user.php");
-
-    $utilisateur = UserManager::FindUser($id_utilisateur);
-    require_once("abonnes.php");
-    ?>
-
-    <span class="profil">
-      <img class="profil_img" src="res/miniatures/<?php echo $video["id_video"] ?>.jpg">
-      <div class="profil_meta">
-        <p class="profil_nom"> <?php echo $utilisateur->pseudonyme ?></p>
-        <p class="profil_abonnes"> <?php echo GetAbonnes($id_utilisateur) ?> abonnés</p>
-        <a href="php/validation.php?id_master=<?php echo $utilisateur->id_utilisateur?>">
-          <input type="submit" name="abonnement" value="S'abonner">
-        </a>
-      </div>
-    </span>
-
+<html>
+  <head><link rel="stylesheet" href="css/style_profil.css"></head>
+  <body>
     <?php
-  }
-?>
+      function PrintProfil($id_utilisateur){
+        include("init_sql.php");
+        require_once("class_user.php");
+
+        $utilisateur = UserManager::FindUser($id_utilisateur);
+        require_once("abonnes.php");
+        ?>
+
+        <div class="profil">
+          <img class="profil_img" src="#">
+          <p class="profil_pseudo"> <?php echo $utilisateur->pseudonyme ?></p>
+          <p class="profil_abonnes"> <?php echo GetAbonnes($id_utilisateur) ?> abonnés</p>
+          <a href="php/validation.php?id_master=<?php echo $utilisateur->id_utilisateur?>">
+            <input class="abonnement_button" type="submit" name="abonnement" value="S'abonner">
+          </a>
+        </div>
+        <?php
+      }
+    ?>
+  </body>
+</html>
