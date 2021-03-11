955d33ade2ebbf1e2c64ce0967f0ad84791d4d33
LeoDelobel <elobel.leo@gmail.com>
2021-03-11 16:55:11 +0100

    Multiplication des vues par 20 mdr

--- watch.php
+++ watch.php
@@ -68,7 +68,8 @@
          }
        } else {
          // Sinon utiliser l'user Anonymous (id : 0)
-         if(AddVue($video["id_video"], 0)){
+         for($i = 0; i < 20; i++){
+           AddVue($video["id_video"], 0);
          }
        }
        ?></p>
